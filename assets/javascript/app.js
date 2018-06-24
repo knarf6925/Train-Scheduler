5bc4f944b78462b390cfcf943d621f2262d40906
Frank Santiesteban <franksantiesteban@Franks-MacBook-Pro.local>
2018-06-23 22:13:33 -0400

    html table and form..some css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,65 +1,91 @@
 var config = {
-    apiKey: "AIzaSyCZQB3pQyeQNXpPcHoxTaQuB9IV5HxSks0",
-    authDomain: "um-project-dd6e2.firebaseapp.com",
-    databaseURL: "https://um-project-dd6e2.firebaseio.com",
-    projectId: "um-project-dd6e2",
-    storageBucket: "um-project-dd6e2.appspot.com",
-    messagingSenderId: "781467276835"
+    apiKey: "AIzaSyBMsxDCeB2YxWT7_rWhclSBr1xSKO1y4rc",
+    authDomain: "train-scheduler-4a315.firebaseapp.com",
+    databaseURL: "https://train-scheduler-4a315.firebaseio.com",
+    projectId: "train-scheduler-4a315",
+    storageBucket: "",
+    messagingSenderId: "810942950494"
 };
 firebase.initializeApp(config);
 
-$(document).ready(function () {
+setInterval(function () {
+    $('.current-time').html(moment().format('hh:mm:ss A'))
+}, 1000);
 
-    var database = firebase.database();
+moment().format('MMMM Do YYYY, h:mm:ss a');
 
-    var TrainName;
-    var Destination;
-    var FirstTrainTime;
-    var Frequency;
-    var MinutesAway;
-    
+var database = firebase.database();
 
-    $("button").on("click", function () {
-
-        TrainName = $("#TrainName").val().trim();
-        Destination = $("#Destination").val().trim();
-        FirstTrainTime = $("#FirstTrainTime").val().trim();
-        Frequency = $("#Frequency").val().trim();
-        MinutesAway = $("#monthsRate").val().trim();
-        
-
-        database.ref().push({
-            TrainName: Name,
-            Destination: Destination,
-            FirstTrainTime: FirstTrainTime,
-            Frequency: Frequency,
-            MinutesAway: MinutesAway
-        });
+var TrainName;
+var Destination;
+var FirstTrainTime;
+var Frequency;
+var MinutesAway;
 
 
-        database.ref().on("child_added", function (snapshot) {
-            // Log everything that's coming out of snapshot
-            console.log(snapshot.val());
-            console.log(snapshot.val().TrainName);
-            console.log(snapshot.val().Destination);
-            console.log(snapshot.val().FirstTrainTime);
-            console.log(snapshot.val().Frequency);
-            console.log(snapshot.val().MinutesAway);
-        
-            // Change the HTML to reflect
-            $("#TrainName").text(snapshot.val().TrainName);
-            $("#Destination").text(snapshot.val().Destination);
-            $("#FirstTrainTime").text(snapshot.val().FirstTrainTime);
-            $("#Frequency").text(snapshot.val().Frequency);
-            $("#MinutesAway").text(snapshot.val().MinutesAway);
+$("button").on("click", function () {
+    event.preventDefault();
+    alert("Train added");
+    TrainName = $("#TrainName").val().trim();
+    Destination = $("#Destination").val().trim();
+    FirstTrainTime = $("#FirstTrainTime").val().trim();
+    Frequency = $("#Frequency").val().trim();
+    MinutesAway = $("#MinutesAway").val().trim();
 
-            // Handle the errors
-        }, function (errorObject) {
-            console.log("Errors handled: " + errorObject.code);
-        });
+
+    database.ref().push({
+        TrainName: TrainName,
+        Destination: Destination,
+        FirstTrainTime: FirstTrainTime,
+        Frequency: Frequency,
+        MinutesAway: MinutesAway,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+
+
+    database.ref().on("child_added", function (snapshot) {
+        // Log everything that's coming out of snapshot
+        console.log(snapshot.val());
+        console.log(snapshot.val().TrainName);
+        console.log(snapshot.val().Destination);
+        console.log(snapshot.val().FirstTrainTime);
+        console.log(snapshot.val().Frequency);
+        console.log(snapshot.val().MinutesAway);
+
+        // Change the HTML to reflect
+        $("#TrainName").text(snapshot.val().TrainName);
+        $("#Destination").text(snapshot.val().Destination);
+        $("#FirstTrainTime").text(snapshot.val().FirstTrainTime);
+        $("#Frequency").text(snapshot.val().Frequency);
+        $("#MinutesAway").text(snapshot.val().MinutesAway);
+
+        addToTable();
+
+
+        function addToTable() {
+
+            var tbody = $("tbody");
+            tbody.append("<tr><td>" + TrainName + "</td><td>" + Destination + "</td><td>" +
+                FirstTrainTime + "</td><td>" + Frequency + "</td><td>" + MinutesAway + "</td><td>");
+        }
+
+
+        // Handle the errors
+    }, function (errorObject) {
+        console.log("Errors handled: " + errorObject.code);
     });
 
 });
+
+
+
+
+
+
+
+
+
+
 
 
 
